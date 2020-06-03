3703e9ed0de69d6a5d9827d3448c294354c943db
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2020-06-02 22:44:24 -0300

    Add feature to create a new repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,36 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+    // TODO
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+    const { title, url, techs } = request.body;
+
+    const repository = {
+        id: uuid(),
+        title,
+        url,
+        techs,
+        likes: 0,
+    };
+
+    repositories.push(repository);
+
+    return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+    // TODO
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+    // TODO
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+    // TODO
 });
 
 module.exports = app;
